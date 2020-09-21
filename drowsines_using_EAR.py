286d743e8e0d542c3fbd879e716bce8d8df2ddb4
Charan-7406 <60846132+Charan-7406@users.noreply.github.com>
2020-09-21 12:25:34 +0530

    Add files via upload

---
+++ drowsines_using_EAR.py
@@ -0,0 +1,117 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from threading import Thread
+import numpy as np
+from imutils import face_utils
+import playsound
+import imutils
+import time
+import dlib
+import cv2
+
+def sound_alarm(path):
+	# play an alarm sound
+	playsound.playsound("C:/Users/Lenovo/PycharmProjects/new_proj/alarm.wav")
+
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    C = dist.euclidean(eye[0], eye[3])
+
+    ear = (A + B) / (2.0 * C)
+
+    return ear
+
+def final_ear(shape):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+
+    ear = (leftEAR + rightEAR) / 2.0
+    return (ear, leftEye, rightEye)
+
+
+def main():
+    EYE_AR_THRESH = 0.3
+    EYE_AR_CONSEC_FRAMES = 30
+    ALARM_ON = False
+    COUNTER = 0
+
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+    # print("-> Starting Video Stream")
+    vs = VideoStream(0).start()
+    # vs= VideoStream(usePiCamera=True).start()       //For Raspberry Pi
+    time.sleep(1.0)
+
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # detect faces in the grayscale frame
+        rects = detector(gray, 0)
+
+        # loop over the face detections
+        for rect in rects:
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            eye = final_ear(shape)
+            ear = eye[0]
+            leftEye = eye[1]
+            rightEye = eye[2]
+
+
+
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+            if ear < EYE_AR_THRESH:
+                COUNTER += 1
+
+                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                    if not ALARM_ON:
+                        ALARM_ON = True
+                        t = Thread(target=sound_alarm,
+                                   args=(["alarm"],))
+                        t.deamon = True
+                        t.start()
+                    cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
+            else:
+                COUNTER = 0
+                ALARM_ON = False
+
+            cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
+        # show the frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
+
+if __name__ == '__main__':
+    main()
